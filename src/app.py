097128aac0766ce396cc8cd3dfe3b7b95cb0d3b9
Wilson <ww373@cornell.edu>
2022-12-02 22:15:53 -0500

    backend

---
+++ src/app.py
@@ -0,0 +1,306 @@
+from db import db
+from db import UserAsset
+from db import ItemAsset
+from db import Item
+from db import User
+
+from flask import Flask
+
+import json
+from flask import request
+import os
+
+
+app = Flask(__name__)
+db_filename = "IthacaMarketplace.db"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps(data), code
+
+
+def failure_response(message, code=404):
+    return json.dumps({"error": message}), code   
+
+
+# your routes here
+"""
+Upload pfp
+Upload item image
+get all users
+get user by id
+create user
+update user
+delete user
+delete user
+get all items
+get all items from a user
+get an item by id
+create an item
+update an item
+delete an item
+"""
+
+@app.route("/")
+def null():
+    """
+    Endpoint that does nothing
+    """
+
+    return "", 200
+
+@app.route("/upload/users/<int:user_id>/", methods=["POST"])
+def upload_pfp(user_id):
+    """
+    Endpoint for uploading an image to AWS given its base64 form,
+    then storing/returning the URL of that image. 
+    Uploads an image for the profile picture of a user.
+    """
+    body = json.loads(request.data)
+
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    image_data = body.get("image_data")
+    if image_data is None:
+        return failure_response("No base64 image passed in!")
+
+    # create UserAsset object
+    asset = UserAsset(image_data = image_data, user_id = user_id)
+    db.session.add(asset)
+     
+    #user.userasset_id = asset.id
+    
+    db.session.commit()
+    return success_response(asset.serialize(), 201)
+
+@app.route("/upload/users/<int:user_id>/", methods=["DELETE"])
+def delete_pfp(user_id):
+    """
+    Endpoint for deleting a user's profile picture.
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    for userasset in UserAsset.query.all():
+        db.session.delete(userasset)
+    db.session.commit()
+    return success_response(user.serialize())
+
+@app.route("/upload/items/<int:item_id>/", methods=["POST"])
+def upload_item_image(item_id):
+    """
+    Endpoint for uploading an image to AWS given its base64 form,
+    then storing/returning the URL of that image. 
+    Uploads an image for an item that a user is selling.
+    """
+    body = json.loads(request.data)
+
+    item = Item.query.filter_by(id=item_id).first()
+    if item is None:
+        return failure_response("Item not found!")
+
+    image_data = body.get("image_data")
+    if image_data is None:
+        return failure_response("No base64 image passed in!")
+
+    # create ItemAsset object
+    asset = ItemAsset(image_data = image_data, item_id = item_id)
+    db.session.add(asset)
+    db.session.commit()
+    return success_response(asset.serialize(), 201)
+
+@app.route("/upload/items/<int:item_id>/<int:asset_id>/", methods=["DELETE"])
+def delete_item_image(item_id, asset_id):
+    """
+    Endpoint for deleting an image of an item.
+    """
+    item = Item.query.filter_by(id=item_id).first()
+    if item is None:
+        return failure_response("Item not found!")
+
+    item_image = ItemAsset.query.filter_by(id=asset_id).first()
+    if item_image is None:
+        return failure_response("Item image not found!")
+
+    db.session.delete(item_image)
+    db.session.commit()
+    return success_response(item.serialize())
+
+@app.route("/users/")
+def get_all_users():
+    """
+    Endpoint for getting all users.
+    """
+    users = [user.serialize() for user in User.query.all()]
+    return success_response({"Users":users})
+
+@app.route("/users/<int:user_id>/")
+def get_user(user_id):
+    """
+    Endpoint for getting a user by id.
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    return success_response(user.serialize())
+
+@app.route("/users/", methods=["POST"])
+def create_user():
+    """
+    Endpoint for creating a new user.
+    """
+
+    body = json.loads(request.data)
+    name = body.get("name")
+    if name == None:
+        return failure_response("Name was not provided", 400)
+    description = body.get("description")
+    userasset_id = body.get("userasset_id") # Empty to begin with bc no pfp uploaded yet
+    userasset = UserAsset.query.filter_by(id=userasset_id).first()
+    if type(userasset_id) == int and userasset == None:
+        return failure_response("Profile picture not found!")
+
+
+    # Create the User object
+    user = User(name = name, description = description)
+    db.session.add(user)
+    db.session.commit()
+    return success_response(user.serialize(), 201)
+
+@app.route("/users/<int:user_id>/", methods=["POST"])
+def update_user(user_id):
+    """
+    Endpoint for updating the name and description of a user by id.
+    """
+    body = json.loads(request.data)
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    if body.get("name") != None:
+        user.name = body.get("name")
+    if body.get("description") == None:
+        user.description = ""
+    else:
+        user.description = body.get("description")
+    db.session.commit()
+
+    return success_response(user.serialize())
+
+@app.route("/users/<int:user_id>/", methods=["DELETE"])
+def delete_user(user_id):
+    """
+    Endpoint for deleting a user by id
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+
+    for userasset in UserAsset.query.all():
+        db.session.delete(userasset)
+
+    db.session.delete(user)
+    db.session.commit()
+    return success_response(user.serialize())
+
+@app.route("/items/")
+def get_all_items():
+    """
+    Endpoint for getting all items.
+    """
+    items = [item.serialize() for item in Item.query.all()]
+    return success_response({"Items":items})
+
+@app.route("/items/users/<int:user_id>/")
+def get_items(user_id):
+    """
+    Endpoint for getting all items of a user by id.
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    return success_response(user.item_serialize())
+
+@app.route("/items/<int:item_id>/")
+def get_item(item_id):
+    """
+    Endpoint for getting an item by id.
+    """
+    item = Item.query.filter_by(id=item_id).first()
+    if item is None:
+        return failure_response("Item not found!")
+    return success_response(item.serialize())
+
+@app.route("/items/users/<int:user_id>/", methods=["POST"])
+def create_item(user_id):
+    """
+    Endpoint for creating a new item for a user
+    """
+    user = User.query.filter_by(id=user_id).first()
+    if user is None:
+        return failure_response("User not found!")
+    
+    body = json.loads(request.data)
+    title = body.get("title")
+    if title == None:
+        return failure_response("Item name not provided!")
+    description = body.get("description")
+    if description == None:
+        description == ""
+    price = body.get("price")
+    if price == None:
+        return failure_response("Price not provided!")
+
+    # Create an item object
+    item = Item(title = title, description = description, price = price, user_id = user_id)
+    db.session.add(item)
+    db.session.commit()
+    return success_response(user.serialize())
+
+@app.route("/users/items/<int:item_id>/", methods=["POST"])
+def update_item(item_id):
+    """
+    Endpoint for updating the title, description, and price of an item by id
+    """
+    body = json.loads(request.data)
+    item = Item.query.filter_by(id=item_id).first()
+    if item is None:
+        return failure_response("Item not found!")
+
+    if body.get("title") != None:
+        item.title = body.get("title")
+    if body.get("description") == None:
+        item.description = ""
+    else:
+        item.description = body.get("description")
+    if body.get("price") != None:
+        item.price = body.get("price")
+    db.session.commit()
+
+    return success_response(item.serialize())
+
+@app.route("/items/<int:item_id>/", methods=["DELETE"])
+def delete_item(item_id):
+    """
+    Endpoint for deleting an item by id
+    """
+    item = Item.query.filter_by(id=item_id).first()
+    if item is None:
+        return failure_response("Item not found!")
+    db.session.delete(item)
+    db.session.commit()
+    return success_response(item.serialize())
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000, debug=True)
---
+++ src/db.py
@@ -0,0 +1,293 @@
+from flask_sqlalchemy import SQLAlchemy
+import base64
+import boto3
+import datetime
+import io
+from io import BytesIO
+from mimetypes import guess_extension, guess_type
+import os
+from PIL import Image
+import random
+import re
+import string
+from dotenv import load_dotenv
+
+load_dotenv() # fix to read environment variables with os
+
+
+db = SQLAlchemy()
+
+EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
+BASE_DIR = os.getcwd()
+S3_BUCKET_NAME_USERS = os.getenv('USERS')
+S3_BUCKET_NAME_ITEMS = os.getenv("ITEMS")
+S3_BASE_URL_USERS = f"https://{S3_BUCKET_NAME_USERS}.s3.us-east-1.amazonaws.com"
+S3_BASE_URL_ITEMS = f"https://{S3_BUCKET_NAME_ITEMS}.s3.us-east-1.amazonaws.com"
+
+temp = S3_BUCKET_NAME_USERS
+
+class UserAsset(db.Model):
+    """
+    One to one relationship with User model.
+    """
+    __tablename__ = "userassets"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    base_url = db.Column(db.String, nullable = True)
+    salt = db.Column(db.String, nullable=False)
+    width = db.Column(db.Integer, nullable = False)
+    height = db.Column(db.Integer, nullable = False)
+    created_at = db.Column(db.DateTime, nullable = False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), unique=True, nullable=True) #nullable can be false
+    user = db.relationship("User", backref="userassets") 
+
+
+    def __init__(self, **kwargs):
+        """
+        Initializes a UserAsset Object.
+        """
+        self.user_id = kwargs.get("user_id")
+        self.create(kwargs.get("image_data"))
+
+
+    def create(self, image_data):
+        """
+        With an image in base64 encoding, 
+        - Generates a random string for the filename.
+        - Decodes the image and attempts to upload it to AWS.
+        - Throws an error if the file type is wrong or uploading fails.
+        """
+        try:
+            ext = guess_extension(guess_type(image_data)[0])[1:]
+            if ext not in EXTENSIONS:
+                raise Exception(f"Unsupported file type: {ext}")
+            
+            salt = "".join(
+                random.SystemRandom().choice(
+                    string.ascii_uppercase + string.digits
+                )
+                for _ in range(16)
+            )
+
+            img_str = re.sub("^data:image/.+;base64,","", image_data)
+            img_data = base64.b64decode(img_str)
+            img = Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL_USERS
+            self.salt = salt
+            self.extension = ext
+            self.width = img.width
+            self.height = img.height
+            self.created_at = datetime.datetime.now()
+
+            img_filename = f"{self.salt}.{self.extension}"
+            self.upload(img, img_filename)
+
+        except Exception as e:
+            print(f"Error when creating image: {e}")
+
+    def upload(self, img, img_filename):
+        """
+        Attempts to upload the image to the specified S3 bucket
+        """
+        try:
+            # save temp on server
+            img_temploc = f"{BASE_DIR}/{img_filename}"
+            img.save(img_temploc)
+
+            # upload to S3
+            s3_client = boto3.client("s3")
+            s3_client.upload_file(img_temploc, S3_BUCKET_NAME_USERS, img_filename)
+
+            # make S3 iage url public
+            s3_resource = boto3.resource("s3")
+            object_acl = s3_resource.ObjectAcl(S3_BUCKET_NAME_USERS, img_filename)
+            object_acl.put(ACL="public-read")
+
+            # remove image from server
+            os.remove(img_temploc)
+
+        except Exception as e:
+            print(f"Error when uploading image: {e}")
+
+    def serialize(self):
+        """
+        Serializes an Asset object.
+        """
+        return {
+            "url": f"{self.base_url}/{self.salt}.{self.extension}",
+            "created_at": str(self.created_at)
+        }
+
+class ItemAsset(db.Model):
+    """
+    Many to one relationship with Item model.
+    """
+    __tablename__ = "itemassets"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    base_url = db.Column(db.String, nullable = True)
+    salt = db.Column(db.String, nullable=False)
+    width = db.Column(db.Integer, nullable = False)
+    height = db.Column(db.Integer, nullable = False)
+    created_at = db.Column(db.DateTime, nullable = False)
+    item_id = db.Column(db.Integer, db.ForeignKey("items.id"), nullable=True) #nullable can be false
+
+    def __init__(self, **kwargs):
+        """
+        Initializes an ItemAsset Object.
+        """
+        self.item_id = kwargs.get("item_id")
+        self.create(kwargs.get("image_data"))
+
+
+    def create(self, image_data):
+        """
+        With an image in base64 encoding, 
+        - Generates a random string for the filename.
+        - Decodes the image and attempts to upload it to AWS.
+        - Throws an error if the file type is wrong or uploading fails.
+        """
+        try:
+            ext = guess_extension(guess_type(image_data)[0])[1:]
+            if ext not in EXTENSIONS:
+                raise Exception(f"Unsupported file type: {ext}")
+            
+            salt = "".join(
+                random.SystemRandom().choice(
+                    string.ascii_uppercase + string.digits
+                )
+                for _ in range(16)
+            )
+
+            img_str = re.sub("^data:image/.+;base64,","", image_data)
+            img_data = base64.b64decode(img_str)
+            img = Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL_ITEMS
+            self.salt = salt
+            self.extension = ext
+            self.width = img.width
+            self.height = img.height
+            self.created_at = datetime.datetime.now()
+
+            img_filename = f"{self.salt}.{self.extension}"
+            self.upload(img, img_filename)
+
+        except Exception as e:
+            print(f"Error when creating image: {e}")
+
+    def upload(self, img, img_filename):
+        """
+        Attempts to upload the image to the specified S3 bucket
+        """
+        try:
+            # save temp on server
+            img_temploc = f"{BASE_DIR}/{img_filename}"
+            img.save(img_temploc)
+
+            # upload to S3
+            s3_client = boto3.client("s3")
+            s3_client.upload_file(img_temploc, S3_BUCKET_NAME_ITEMS, img_filename)
+
+            # make S3 iage url public
+            s3_resource = boto3.resource("s3")
+            object_acl = s3_resource.ObjectAcl(S3_BUCKET_NAME_ITEMS, img_filename)
+            object_acl.put(ACL="public-read")
+
+            # remove image from server
+            os.remove(img_temploc)
+
+        except Exception as e:
+            print(f"Error when uploading image: {e}")
+
+    def serialize(self):
+        """
+        Serializes an Asset object.
+        """
+        return {
+            "url": f"{self.base_url}/{self.salt}.{self.extension}",
+            "created_at": str(self.created_at)
+        }
+
+class User(db.Model):
+
+    """
+    User model.
+    One to many relationship with Items model.
+    One to one relationship with UserAsset model.
+    """
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    name = db.Column(db.String, nullable = False)
+    description = db.Column(db.String, nullable = True) #with contact info? May have no description
+    userasset = db.relationship("UserAsset", backref="users", uselist=False, cascade="delete") 
+    items = db.relationship("Item", cascade="delete")
+    
+
+    #FORIEGN KEY TO IMAGE 
+
+    #username?
+    #reputation? points given by buyer if good sale. -1 if bad sale.
+
+    def __init__(self, **kwargs):
+        """
+        Creates a User object
+        """
+        self.name = kwargs.get("name")
+        self.description = kwargs.get("description", "")
+    
+    def serialize(self):
+        """
+        Serializes a User object
+        """
+        return {
+            "id":self.id,
+            "name": self.name,
+            "description":self.description,
+            "items": [x.serialize() for x in self.items]
+        }
+
+    def item_serialize(self):
+        """
+        Serializes a User object with only items
+        """
+        return {
+            "items": [x.serialize() for x in self.items]
+        }
+
+class Item(db.Model):
+
+    """
+    Item model.
+    Many to one relationship with Users model.
+    One to many relationship with ItemAssets model.
+    """
+    __tablename__ = "items"
+    id = db.Column(db.Integer, primary_key = True, autoincrement = True)
+    title = db.Column(db.String, nullable = True)
+    description = db.Column(db.String, nullable = True)
+    price = db.Column(db.Integer, nullable = True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    images = db.relationship("ItemAsset", cascade="delete")
+
+    def __init__(self, **kwargs):
+        """
+        Creates an Item object
+        """
+        self.title = kwargs.get("title")
+        self.description = kwargs.get("description")
+        self.price = kwargs.get("price")
+        self.user_id = kwargs.get("user_id")
+
+    def serialize(self):
+        """
+        Serializes an Item object
+        """
+        return {
+            "id": self.id,
+            "title": self.title,
+            "description": self.description,
+            "price": self.price,
+            "imageasset_id_list": [x.id for x in self.images]
+        }
+
+
